9745367b0382ebc3ae4c2d00a97e3b1e6531bd7f
jlmarkowitz <jlmarkowitz@gmail.com>
2023-03-27 13:54:19 -0400

    hamburger icon for JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("I've done it!");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
